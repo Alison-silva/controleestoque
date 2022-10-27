75f9fd4d95ff330b85ee4313de8418340c448637
Alison-silva <alison.ssilva@hotmail.com>
2022-10-26 23:39:42 -0300

    Configuração do projeto, libs, banco de dados e pastas

--- controleestoque/src/main/java/com/alison/controleestoque/ControleestoqueApplication.java
+++ controleestoque/src/main/java/com/alison/controleestoque/ControleestoqueApplication.java
@@ -1,9 +1,23 @@
 package com.alison.controleestoque;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.alison.controleestoque.model"})
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.alison.controleestoque.repositories"})
+@EnableTransactionManagement
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class ControleestoqueApplication {
 
 	public static void main(String[] args) {
