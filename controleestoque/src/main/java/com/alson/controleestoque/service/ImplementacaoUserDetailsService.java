80f5aa74b810b9f3fe604738b39227345b7d661b
Alison-silva <alison.ssilva@hotmail.com>
2022-11-08 16:34:32 -0300

    Criando a autenticação com jwt web token

--- controleestoque/src/main/java/com/alson/controleestoque/service/ImplementacaoUserDetailsService.java
+++ controleestoque/src/main/java/com/alson/controleestoque/service/ImplementacaoUserDetailsService.java
@@ -1,15 +1,31 @@
 package com.alson.controleestoque.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-public class ImplementacaoUserDetailsService implements UserDetailsService{
+import com.alison.controleestoque.model.Usuario;
+import com.alison.controleestoque.repositories.UsuarioRepository;
+
+@Service
+public class ImplementacaoUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+
+		Usuario usuario = usuarioRepository.findUserByLogin(username);
+
+		if (usuario == null) {
+			throw new UsernameNotFoundException("Usuário não foi encontrado");
+		}
+
+		return new User(usuario.getLogin(), usuario.getSenha(), usuario.getAuthorities());
 	}
 
 }
