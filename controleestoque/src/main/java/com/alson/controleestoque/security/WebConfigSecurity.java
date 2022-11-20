67611c72281e0074d8b0be4aee73573bf2c57a59
Alison-silva <alison.ssilva@hotmail.com>
2022-11-20 00:28:17 -0300

    Configuração de cors, e autenticação da rota

--- controleestoque/src/main/java/com/alson/controleestoque/security/WebConfigSecurity.java
+++ controleestoque/src/main/java/com/alson/controleestoque/security/WebConfigSecurity.java
@@ -2,6 +2,7 @@ package com.alson.controleestoque.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,8 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
 		
 		.disable().authorizeRequests().antMatchers("/").permitAll()
 		.antMatchers("/index").permitAll()
+		
+		.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 		
 		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
 		
